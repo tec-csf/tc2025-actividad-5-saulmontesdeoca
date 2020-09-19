50109ffdbe2e4d45b131d74f70287d03cd0d62bd
Saul Montes De Oca <mr.sauce97@gmail.com>
2020-09-18 22:08:09 -0500

    🚀 Done

--- main.c
+++ main.c
@@ -13,18 +13,38 @@
 int main(int argc, char const *argv[])
 {
     int children = 0;
-    if(argc > 1)
-    {
-        children = atoi(argv[1]);
-    } else
-    {
-        // No number of child processes
-        printf("Please enter number of children processes to exeute next time.\n");
-        return -1;
+
+    char *pvalue = NULL;
+    int p;
+    while ((p = getopt (argc, argv, "n:")) != -1)
+        switch (p)
+        {
+        case 'n':
+            pvalue = optarg;
+            break;
+        case '?':
+            if (optopt == 'n')
+                fprintf (stderr, "Opción -%c requiere un argumento.\n", optopt);
+            else if (isprint (optopt))
+                fprintf (stderr, "Opción desconocida '-%c'.\n", optopt);
+            else
+                fprintf (stderr,
+                         "Opción desconocida '\\x%x'.\n",
+                         optopt);
+            return 1;
+        default:
+            abort ();
     }
+
+    for (int index = optind; index < argc; index++)
+        printf ("El argumento no es una opción válida %s\n", argv[index]);
+    
+    children = atoi(pvalue);
 
     // Processes
     pid_t pid;
+    pid_t *pidChildProcesses = (pid_t *) malloc((children-1) * sizeof(pid_t));
+    pid_t *aux = pidChildProcesses;
 
     // Se declara un array de tuberias, cada dos espacios representa una
     // tuberia y es por donde va a ir viajando el testigo
@@ -40,29 +60,58 @@ int main(int argc, char const *argv[])
     // Declarando char input
     char input;
 
+
     int i = 1;
-    while (i <= children)
+    int j = 0;
+    while (i < children)
     {
         pid = fork();
-
         if (pid == 0)
         {
-            // Se cierra write de la tuberia actual
-            close(tuberia[(i*2 - 1)]);
-            // Se lee de la tuberia actual
-            read(tuberia[i*2 - 2], &input, sizeof(char));
+            if(i == children -1){
+                while(j < 3){
+                    // Se cierra write de la tuberia actual
+                    close(tuberia[(i*2 + 1)]);
+                    // Se lee de la tuberia actual
+                    read(tuberia[i*2], &input, sizeof(char));
 
-            printf("—-> Soy el proceso %d con PID %d  y recibí el testigo '%c', el cual tendré por 5 segundos\n", i, getpid(), input);
+                    printf("—-> Soy el proceso %d con PID %d  y recibí el testigo '%c', el cual tendré por 5 segundos\n", i, getpid(), input);
 
-            sleep(5); // Holding
+                    sleep(5); // Holding
 
-            // Cerrando read de siguientetuberia
-            close(tuberia[(i*2)]);
+                    // Cerrando read de siguientetuberia
+                    close(tuberia[0]);
 
-            // Escribiendo en siguiente tuberia
-            write(tuberia[i*2 + 1], &input, sizeof(char));
+                    // Escribiendo en siguiente tuberia
+                    write(tuberia[1], &input, sizeof(char));
 
-            printf("<—- Soy el proceso %d con PID %d y acabo de enviar el testigo '%c'\n", i, getpid(), input);
+                    printf("<—- Soy el proceso %d con PID %d y acabo de enviar el testigo '%c'\n", i, getpid(), input);
+                    j++;
+                }
+
+            } else
+            {
+                while(j < 3){
+                    // Se cierra write de la tuberia actual
+                    close(tuberia[(i*2 + 1)]);
+                    // Se lee de la tuberia actual
+                    read(tuberia[i*2], &input, sizeof(char));
+
+                    printf("—-> Soy el proceso %d con PID %d  y recibí el testigo '%c', el cual tendré por 5 segundos\n", i, getpid(), input);
+
+                    sleep(5); // Holding
+
+                    // Cerrando read de siguientetuberia
+                    close(tuberia[((i*2) + 2)]);
+
+                    // Escribiendo en siguiente tuberia
+                    write(tuberia[(i*2) + 3], &input, sizeof(char));
+
+                    printf("<—- Soy el proceso %d con PID %d y acabo de enviar el testigo '%c'\n", i, getpid(), input);
+                    j++;
+                }
+            }
+            exit(0);
         }
         else if (pid == -1)
         {
@@ -72,25 +121,56 @@ int main(int argc, char const *argv[])
         }
         else
         {
-            // Si es el primer proceso, el padre de todos
+            // Si es el proceso padre escribe la primera vez el testigo
             if (i == 1)
             {
                 // Cerramos read de la primera tuberia
-                close(tuberia[0]);
+                // close(tuberia[0]);
                 // Escribimos en la primera tuberia
-                write(tuberia[1], &testigo, sizeof(char));
-                printf("Soy el proceso padre con PID = %d y envío '%c'\n", pid, testigo);
+                // write(tuberia[1], &testigo, sizeof(char));
+                // printf("—-> Soy el proceso %d con PID %d  y recibí el testigo '%c', el cual tendré por 5 segundos\n", i, getpid(), input);
+                // printf("Soy el proceso con PID = %d y envío '%c'\n", pid, input);
+                *aux = pid;
             }
-            // cada hijo al tener otro hijo hace break y no seguira en el loop
-            break;
         }
         i++;
+        aux++;
     }
 
 
-    // Cada padre espera a su hijo
-    waitpid(pid, NULL, 0);
+    printf("Escribiendo testigo:\n");
+    write(tuberia[1], &testigo, sizeof(char));
 
+    //Lectura y escritura del padre
+    while(j < 3){
+        // Se cierra write de la tuberia actual
+        close(tuberia[1]);
+        // Se lee de la tuberia actual
+        read(tuberia[0], &input, sizeof(char));
+
+        printf("—-> Soy el proceso 0 con PID %d  y recibí el testigo '%c', el cual tendré por 5 segundos\n", getpid(), input);
+
+        sleep(5); // Holding
+
+        // Cerrando read de siguientetuberia
+        close(tuberia[2]);
+
+        // Escribiendo en siguiente tuberia
+        write(tuberia[3], &input, sizeof(char));
+
+        printf("<—- Soy el proceso 0 con PID %d y acabo de enviar el testigo '%c'\n", getpid(), input);
+        j++;
+    }
+
+    // Proceso padre espera a todos sus hijos a que terminen
+    aux = pidChildProcesses;
+    pid_t *final = pidChildProcesses + (children-1);
+    for (; aux < final; ++aux)
+    {
+        waitpid(*aux, NULL, 0);
+    }
+    
+    free(pidChildProcesses);
 
     return 0;
 }
